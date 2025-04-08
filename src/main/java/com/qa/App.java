c287f05b32fdc202597537bd90291d8a60b54c06
Christian <christian@qa.com>
2025-04-08 11:57:04 +0100

    changed names

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -9,10 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println(sayHello());
-        System.out.println(sayHelloToSomeone("Alice"));
-        System.out.println(sayHelloToSomeone("Bob"));
-        System.out.println(sayHelloToSomeone("Charlie"));
-        System.out.println(sayHelloToSomeone("Zena"));
+        System.out.println(sayHelloToSomeone("Dave"));
+        System.out.println(sayHelloToSomeone("Daz"));
+        System.out.println(sayHelloToSomeone("Del"));
+        System.out.println(sayHelloToSomeone("Dick"));
         System.out.println(sayGoodbye());
     }
 
